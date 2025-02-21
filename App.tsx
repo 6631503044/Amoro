d75660062202194dbe945bf4e82e1e0e3b414f8e
bearbabe0420 <6631503111@lamduan.mfu.ac.th>
2025-02-21 20:27:12 +0700

    add icon all activity

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { View, Text, StyleSheet, Image, Dimensions, Animated } from 'react-native';
 import MoodBoxSingle from './components/MoodBox/MoodBoxSingle';
 import MoodBoxCouple from './components/MoodBox/MoodBoxCouple';
 import TaskBoxSingle from './components/TaskBox/TaskBoxSingle';
 import NotificationBoxCouple from './components/NotificationBox/NotificationBoxCouple';
 import ReviewBoxSingle from './components/ReviewBox/ReviewBox';
+=======
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import InputField from './components/InputFields/InputField'; // Path to InputField component
+>>>>>>> Stashed changes
 
 const { width, height } = Dimensions.get('window');
 import { primarycolor } from './assets/img/styles';
 
 const App = () => {
   return (
+<<<<<<< Updated upstream
     
     <View style={styles.container}>
             <ReviewBoxSingle 
         reviewText="The movie in that scene was too big. I want to ride a big dragon. 🐉🔥🔥🔥"
       />
+=======
+    <View style={styles.container}>
+      {/* InputField with an icon */}
+      <InputField icon={require('./assets/icon/backicon.svg')} />
+
+      {/* InputField without an icon */}
+      <InputField />
+>>>>>>> Stashed changes
     </View>
   );
 };
@@ -25,6 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+<<<<<<< Updated upstream
     position: 'relative', 
     
   },
@@ -33,6 +48,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: primarycolor,
     zIndex: 1,
+=======
+    backgroundColor: '#f5f5f5',
+>>>>>>> Stashed changes
   },
 });
 
